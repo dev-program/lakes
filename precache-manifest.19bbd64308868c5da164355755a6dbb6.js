c93ef66d20094863b4bd64a0cb9ca203df5ef9fb
dev-program <andres.vicedo@lspu.edu.ph>
2022-09-15 10:38:00 +0800

    Updates

---
+++ precache-manifest.19bbd64308868c5da164355755a6dbb6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a97eb4d89f0c682156",
+    "url": "/lakes/static/css/main.d7d40efc.chunk.css"
+  },
+  {
+    "revision": "93a97eb4d89f0c682156",
+    "url": "/lakes/static/js/main.90f4cb81.chunk.js"
+  },
+  {
+    "revision": "44c84897775ec3259a23",
+    "url": "/lakes/static/js/runtime~main.79b52720.js"
+  },
+  {
+    "revision": "555561b27a71273283ee",
+    "url": "/lakes/static/css/2.94fe32d2.chunk.css"
+  },
+  {
+    "revision": "555561b27a71273283ee",
+    "url": "/lakes/static/js/2.5f0ab0a0.chunk.js"
+  },
+  {
+    "revision": "6ef37438c4718a381467",
+    "url": "/lakes/static/js/3.3af468aa.chunk.js"
+  },
+  {
+    "revision": "4f687bf7b8c45beb1789a99e329e2b2e",
+    "url": "/lakes/static/media/CLSD.4f687bf7.png"
+  },
+  {
+    "revision": "eea4c4ef2470248448b1b856258f5740",
+    "url": "/lakes/static/media/LSPU.eea4c4ef.png"
+  },
+  {
+    "revision": "8ff526445e145b9bef8ee190ca9fe2b7",
+    "url": "/lakes/static/media/DOST.8ff52644.png"
+  },
+  {
+    "revision": "2eea01cd3eb607a6abbbe143f79bf486",
+    "url": "/lakes/static/media/PCIEERD.2eea01cd.png"
+  },
+  {
+    "revision": "32a9bde8527d48e4205375da7261acf9",
+    "url": "/lakes/static/media/PCAARRD.32a9bde8.png"
+  },
+  {
+    "revision": "3e07b0ae8c1925aa9237f1b6010d8d9f",
+    "url": "/lakes/static/media/NRCP.3e07b0ae.png"
+  },
+  {
+    "revision": "9fbfa6aaf0860f11287af5908b390b67",
+    "url": "/lakes/static/media/DA-BFAR.9fbfa6aa.png"
+  },
+  {
+    "revision": "03369e4a1914a2095405d2f82315cf7f",
+    "url": "/lakes/static/media/UPLB.03369e4a.png"
+  },
+  {
+    "revision": "aa4f8692cce70069986f7b0923b6e7ae",
+    "url": "/lakes/static/media/LAGUNA.aa4f8692.png"
+  },
+  {
+    "revision": "2fcf4cbcdcb676e0e4b43053a393eb5e",
+    "url": "/lakes/static/media/clsd-header.2fcf4cbc.png"
+  },
+  {
+    "revision": "cde75cb7ce4d280ddb6f53284406fc96",
+    "url": "/lakes/static/media/DENR.cde75cb7.png"
+  },
+  {
+    "revision": "02bed7fd841707229027439dbe5246d4",
+    "url": "/lakes/static/media/S4CP.02bed7fd.png"
+  },
+  {
+    "revision": "43b43a8eed47ca6d73aedc1431f8c8ec",
+    "url": "/lakes/index.html"
+  }
+];
